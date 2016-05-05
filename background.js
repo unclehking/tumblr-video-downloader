9f35b5d2750f622fba3d67b79e26271fe7c9621e
unclehking <513935463@qq.com>
2016-05-05 10:48:05 +0800

    tumblr

---
+++ background.js
@@ -0,0 +1,19 @@
+﻿/*HKing 2016-04-29*/
+chrome.contextMenus.create({
+	"title": "迅雷下载",
+	"contexts":["video"],
+	"documentUrlPatterns":["*://*.tumblr.com/*"]
+});
+chrome.contextMenus.onClicked.addListener(function(info,tab){
+	var rUrl = 'https://vt.tumblr.com/'+
+			   /tumblr_.*/.exec(info.srcUrl)[0].replace(/\//g,"_")+'.mp4',
+	thunderUrl = 'thunder://'+window.btoa('AA'+rUrl+'ZZ');
+	chrome.tabs.sendMessage(
+		tab.id,
+		{
+			url:thunderUrl,
+			oldUrl:info.srcUrl
+		},
+		function(response){}
+	);
+});
---
+++ main.js
@@ -0,0 +1,14 @@
+﻿/*HKing 2016-04-29*/
+var ifr;
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse){
+		var v = document.querySelector("video").querySelector("source");
+		if(v.src==request.oldUrl || self==top){
+			var isFirst;
+			ifr = ifr || (isFirst=true,document.createElement('iframe'));
+			ifr.src = request.url;
+			isFirst && document.body.appendChild(ifr);
+		};
+	    return false;
+	}
+);
