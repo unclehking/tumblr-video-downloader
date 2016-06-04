987d8095c8ddad65260e1c5a1738bb63005d49a7
unclehking <513935463@qq.com>
2016-06-04 17:34:21 +0800

    es6 use strict

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 ﻿/*HKing 2016-04-29*/
 var ifr, copyUrl = chrome.i18n.getMessage("copyUrl"),
-copyed = chrome.i18n.getMessage("copyed");
+copyed = chrome.i18n.getMessage("copied");
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse){
 		var v = document.querySelector("video").querySelector("source");
@@ -22,10 +22,10 @@ document.addEventListener('mouseover', (e) =>{
 			    rUrl = `https://vt.tumblr.com/${/tumblr_.*/.exec(oUrl)[0].replace(/\//g,"_")}.mp4`,
 			    hkingWrap = `<div class="hkingWrap">
 								<input type="text" value="${rUrl}" />${copyUrl}
-								<div>${copyed}</div>
+								<div>${copied}</div>
 							</div>`;
 			jQuery($t).after(hkingWrap);
-			$t.next().click( (e)=>{
+			$t.next().click(function(e){
 				let copyTextarea = jQuery(this).find("input");
 				copyTextarea.select();
 				document.execCommand('copy');
