5b68762707a1b41404ec43f7df691ebb411e9964
unclehking <513935463@qq.com>
2016-12-05 17:41:09 +0800

    new

--- res/js/background.js
+++ res/js/background.js
@@ -1,70 +1,32 @@
-﻿/*HKing 2016-06-25*/
-// const thunderDl =  chrome.i18n.getMessage("thunderDl");
-// const download = chrome.i18n.getMessage("download");
-// const url = "www.tumblr.com";
-// console.log(thunderDl);
-// chrome.contextMenus.ACTION_MENU_TOP_LEVEL_LIMIT = 5;
-// chrome.contextMenus.create({
-// 	"title": thunderDl,
-// 	"contexts":["video"],
-// 	"id":"thunderDownload",
-// 	"documentUrlPatterns":["*://*.tumblr.com/*"]
-// });
-// chrome.contextMenus.create({
-// 	"title": download,
-// 	"contexts":["video"],
-// 	"id":"chromeDownload",
-// 	"documentUrlPatterns":["*://*.tumblr.com/*"]
-// });
-//
-// chrome.contextMenus.onClicked.addListener((info,tab)=>{
-// 	let rUrl = `https://vt.tumblr.com/${/tumblr_.*/.exec(info.srcUrl)[0].replace(/\//g,"_")}.mp4`,
-// 	thunderUrl = `thunder://${window.btoa(`AA${rUrl}ZZ`)}`;
-// 	if(info.menuItemId === "thunderDownload"){
-// 		chrome.tabs.sendMessage(
-// 			tab.id,
-// 			{
-// 				url:thunderUrl,
-// 				oldUrl:info.srcUrl,
-// 				rUrl:rUrl
-// 			},
-// 			response=>{}
-// 		);
-// 	}else{
-// 		chrome.downloads.download(
-// 			{
-// 				url:rUrl
-// 			},
-// 			id=>{}
-// 		);
-// 	}
-//
-// });
+﻿/*HKing 2016-12-5*/
 
-chrome.contextMenus.create({
-	id: "clickme",
-    title: "Click me!",
-  	contexts: ["all"],
-	documentUrlPatterns: ["*://*.tumblr.com/*"]
-});
+browser.runtime.onMessage.addListener(
+	f_nancy = (request, sender, sendResponse) =>{
+		browser.downloads.download(
+			{
+				url: request.url
+			},
+			id=>{}
+		);
+		sendResponse({info:"downloading"});
+	}
+);
 
-
-const  url="d3d3LnR1bWJsci5jb20=";
+const  url="www.tumblr.com";
 browser.browserAction.onClicked.addListener((tab) =>{
 	function logTabs(tabs){
 		for(tab of tabs){
-			if(tab.url.match(atob(url))){
+			if(tab.url.match(url)){
 				logTabs.bTab = true;
 				browser.tabs.update(tab.id, {active: true});
 				break;
 			}
 		}
-		!logTabs.bTab && browser.tabs.create({url:`https://${atob(url)}`});
+		!logTabs.bTab && browser.tabs.create({url:`https://${url}`});
 	}
 	function onError(error) {
 		console.log(`Error: ${error}`);
 	}
 	var querying = browser.tabs.query({currentWindow: true});
 	querying.then(logTabs, onError);
-
 });
--- res/js/main.js
+++ res/js/main.js
@@ -1,38 +1,29 @@
-﻿/*HKing 2016-04-29*/
-// var ifr;
-const copyUrl = chrome.i18n.getMessage("copyUrl"),copied = chrome.i18n.getMessage("copied");
-// chrome.runtime.onMessage.addListener(
-// 	f_nancy = (request, sender, sendResponse) =>{
-// 		var v = document.querySelector("video").querySelector("source");
-// 		if(v.src==request.oldUrl || self==top){
-// 			ifr = ifr || (f_nancy.isFirst=true,document.createElement('iframe'));
-// 			ifr.src = request.url;
-// 			f_nancy.isFirst && document.body.appendChild(ifr);
-// 		};
-// 	    return false;
-// 	}
-// );
+﻿/*HKing 2016-12-5*/
 
+const download = browser.i18n.getMessage("download"), downloading = browser.i18n.getMessage("downloading");
 document.addEventListener('mouseover', (e) =>{
 	if(e.target.nodeName == "VIDEO"){
-
 		let $t = jQuery(e.target);
-
 		if(!$t.next().hasClass('hkingWrap')){
-
 			let oUrl = e.target.querySelector("source").src;
 			let rUrl = `https://vt.tumblr.com/${/tumblr_.*/.exec(oUrl)[0].replace(/\//g,"_")}.mp4`;
 			let hkingWrap = `<div class="hkingWrap">
-								<input type="text" value="${rUrl}" />${copyUrl}
-								<div>${copied}</div>
+								<input type="text" value="${rUrl}" />${download}
+								<div>${downloading}</div>
 							</div>`;
 			jQuery($t).after(hkingWrap);
 			$t.next().click( (event) =>{
-				event.target.querySelector("input").select();
-				document.execCommand('copy');
-				$(event.target).find('div').show().delay(2000).fadeOut();
+				var _target = event.target;
+				browser.runtime.sendMessage(
+					{
+						url:rUrl
+					},
+					response=>{
+						$(_target).find('div').show().delay(2000).fadeOut();
+					}
+				)
+				return false;
 			});
-		}
-
+		};
 	};
 });
