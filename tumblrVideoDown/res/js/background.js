a1f2a7d6b5ddc006474262c5a4f0dbcc462fddaf
unclehking <513935463@qq.com>
2017-01-06 18:06:46 +0800

    newUI

--- tumblrVideoDown/res/js/background.js
+++ tumblrVideoDown/res/js/background.js
@@ -1,44 +1,15 @@
 ﻿/*HKing 2016-06-25*/
-const thunderDl =  chrome.i18n.getMessage("thunderDl");
-const download = chrome.i18n.getMessage("download");
-const url = "www.tumblr.com";
-chrome.contextMenus.ACTION_MENU_TOP_LEVEL_LIMIT = 5;
-chrome.contextMenus.create({
-	"title": thunderDl,
-	"contexts":["video"],
-	"id":"thunderDownload",
-	"documentUrlPatterns":["*://*.tumblr.com/*"]
-});
-chrome.contextMenus.create({
-	"title": download,
-	"contexts":["video"],
-	"id":"chromeDownload",
-	"documentUrlPatterns":["*://*.tumblr.com/*"]
-});
-
-chrome.contextMenus.onClicked.addListener((info,tab)=>{
-	let rUrl = `https://vt.tumblr.com/${/tumblr_.*/.exec(info.srcUrl)[0].replace(/\//g,"_")}.mp4`,
-	thunderUrl = `thunder://${window.btoa(`AA${rUrl}ZZ`)}`;
-	if(info.menuItemId === "thunderDownload"){
-		chrome.tabs.sendMessage(
-			tab.id,
-			{
-				url:thunderUrl,
-				oldUrl:info.srcUrl,
-				rUrl:rUrl
-			},
-			response=>{}
-		);
-	}else{
+chrome.runtime.onMessage.addListener(
+	f_nancy = (request, sender, sendResponse) =>{
 		chrome.downloads.download(
 			{
-				url:rUrl
+				url: request.url
 			},
 			id=>{}
 		);
+		sendResponse({info:"downloading"});
 	}
-
-});
+);
 
 chrome.browserAction.onClicked.addListener( tab =>{
 	chrome.tabs.getAllInWindow(null, f_nancy = tabs =>{
--- tumblrVideoDown/res/js/main.js
+++ tumblrVideoDown/res/js/main.js
@@ -1,17 +1,5 @@
 ﻿/*HKing 2016-04-29*/
-var ifr;
-const copyUrl = chrome.i18n.getMessage("copyUrl"),copied = chrome.i18n.getMessage("copied");
-chrome.runtime.onMessage.addListener(
-	f_nancy = (request, sender, sendResponse) =>{
-		var v = document.querySelector("video").querySelector("source");
-		if(v.src==request.oldUrl || self==top){
-			ifr = ifr || (f_nancy.isFirst=true,document.createElement('iframe'));
-			ifr.src = request.url;
-			f_nancy.isFirst && document.body.appendChild(ifr);
-		};
-	    return false;
-	}
-);
+
 
 document.addEventListener('mouseover', (e) =>{
 	if(e.target.nodeName == "VIDEO"){
@@ -19,16 +7,22 @@ document.addEventListener('mouseover', (e) =>{
 		if(!$t.next().hasClass('hkingWrap')){
 			let oUrl = e.target.querySelector("source").src,
 			    rUrl = `https://vt.tumblr.com/${/tumblr_.*/.exec(oUrl)[0].replace(/\//g,"_")}.mp4`,
-			    hkingWrap = `<div class="hkingWrap" data-url="${rUrl}">
+			    hkingWrap = `<div class="hkingWrap" data-url="${rUrl}" title="Download this video.">
 								<div></div>
+								<p></p>
 							</div>`;
-
-			jQuery($t).after($btn);
+			jQuery($t).after(hkingWrap);
 			$t.next().click( (event) =>{
-				alert(event.target.dataset.url)
-				// event.target.querySelector("input").select();
-				// document.execCommand('copy');
-				// $(event.target).find('div').show().delay(2000).fadeOut();
+				var target = event.target;
+				chrome.runtime.sendMessage(
+					{
+						url:target.dataset.url
+					},
+					response=>{
+						$(target).find("p").show().delay(2000).fadeOut(1000);
+					}
+				)
+				return false;
 			});
 		}
 
